3470b24f82e1e20e2e8fda35c72cae368677ad69
ivanaazuka <ivana.situmorang@amarbank.co.id>
2019-12-06 15:39:36 +0700

    feat(database helper) : add database helper for favorite match

--- app/src/main/java/com/example/footballleaguemvp/data/Match.kt
+++ app/src/main/java/com/example/footballleaguemvp/data/Match.kt
@@ -18,7 +18,23 @@ data class Match(
     val idHomeTeam: String,
     val idAwayTeam: String,
     val strSport: String
-)
+) {
+    companion object {
+        const val TABLE_FAVORITE_MATCH = "TABLE_FAVORITE_MATCH"
+        const val EVENT_ID = "EVENT_ID"
+        const val EVENT_NAME = "EVENT_NAME"
+        const val EVENT_HOME_TEAM = "EVENT_HOME_TEAM"
+        const val EVENT_AWAY_TEAM = "EVENT_AWAY_TEAM"
+        const val EVENT_HOME_SCORE = "EVENT_HOME_SCORE"
+        const val EVENT_AWAY_SCORE = "EVENT_AWAY_SCORE"
+        const val EVENT_DATE = "EVENT_DATE"
+        const val EVENT_TIME = "EVENT_TIME"
+        const val EVENT_ID_HOME_TEAM = "EVENT_ID_HOME_TEAM"
+        const val EVENT_ID_AWAY_TEAM = "EVENT_ID_AWAY_TEAM"
+        const val EVENT_SPORT_NAME = "EVENT_SPORT_NAME"
+
+    }
+}
 
 data class MatchResponse(
     val events: List<Match>
---
+++ app/src/main/java/com/example/footballleaguemvp/utils/databasehelper/DatabaseHelper.kt
@@ -0,0 +1,48 @@
+package com.example.footballleaguemvp.utils.databasehelper
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import com.example.footballleaguemvp.data.Match
+import org.jetbrains.anko.db.*
+
+
+/**
+ * Created by ivanaazuka on 2019-12-06.
+ * Android Engineer
+ */
+ 
+class DatabaseHelper(context: Context) : ManagedSQLiteOpenHelper(context, "FavoriteMatchDb", null, 1) {
+    companion object {
+        private var instance: DatabaseHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): DatabaseHelper {
+            if (instance == null) {
+                instance = DatabaseHelper(ctx.applicationContext)
+            }
+            return instance as DatabaseHelper
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.createTable(Match.TABLE_FAVORITE_MATCH, true,
+            Match.EVENT_ID to TEXT + PRIMARY_KEY,
+            Match.EVENT_NAME to TEXT,
+            Match.EVENT_HOME_TEAM to TEXT,
+            Match.EVENT_AWAY_TEAM to TEXT,
+            Match.EVENT_HOME_SCORE to TEXT,
+            Match.EVENT_AWAY_SCORE to TEXT,
+            Match.EVENT_DATE to TEXT,
+            Match.EVENT_TIME to TEXT,
+            Match.EVENT_ID_HOME_TEAM to TEXT,
+            Match.EVENT_ID_AWAY_TEAM to TEXT,
+            Match.EVENT_SPORT_NAME to TEXT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.dropTable(Match.TABLE_FAVORITE_MATCH, true)
+    }
+}
+
+val Context.database: DatabaseHelper
+ get() = DatabaseHelper.getInstance(applicationContext)
